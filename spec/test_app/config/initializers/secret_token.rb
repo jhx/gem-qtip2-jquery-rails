6b63bcb3f5c8f45b9a0f183bb9284cc16ecc4fcd
Doc Walker <doc.walker@jameshardie.com>
2014-02-18 20:15:43 -0600

    Fix rubocop LineLength offenses

--- spec/test_app/config/initializers/secret_token.rb
+++ spec/test_app/config/initializers/secret_token.rb
@@ -5,4 +5,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TestApp::Application.config.secret_token = '413ea75e7437f3cdcc7df55af98226e2b598813d81baf12ce5c2c6054801dc5c106a0a93540f00968898b2f560176f806a9167689c399f639489be4addad53be'
+TestApp::Application.config.secret_token = '413ea75e7437f3cdcc7df55af98226e' \
+  '2b598813d81baf12ce5c2c6054801dc5c106a0a93540f00968898b2f560176f806a91676' \
+  '89c399f639489be4addad53be'
